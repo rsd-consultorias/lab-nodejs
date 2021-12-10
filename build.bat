afc27b71e2fed3cad1e8d2dc5e556ee3a6393e0c
Rafael Dias <rafael.dias@rsdconsultoria.com.br>
2021-12-10 16:26:16 -0300

    bat

--- build.bat
+++ build.bat
@@ -1,28 +1,4 @@
 cls
 
-echo "################# 1 - Build e Teste do CORE #############"
 cd ./banco-core
-rm -rf ./build
-npm audit fix
-npm i
-npm run build
-npm test
-
-echo "################# 2 - Build da API ######################"
-cd ../banco-api
-rm -rf ./build
-npm audit fix
-npm i
-npm run build
-npx tsoa spec
-npm run swagger-autogen
-
-echo "################# 3 - Build do front-end ################"
-cd ../front-end
-rm -rf ./dist
-npm audit fix
-npm i
-npm run build
-
-echo "################# 4 - Rodar serviços ####################"
-ng serve | node ../banco-api/build/index.js
+npm audit fix && npm i && npm run build && npm test && cd ../banco-api && npm audit fix && npm i && npm run build && npx tsoa spec && npm run swagger-autogen && cd ../front-end && npm audit fix && npm i && npm run build && npx ng serve | node ../banco-api/build/index.js
